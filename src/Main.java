1f310e3bcc422920b9a6d4ad5cd54d9f82b2c9bb
adkow <a.kowalewski.student@gmail.com>
2020-02-16 10:49:20 +0100

    Sprawdzenie pełnoletności.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,21 @@
+import java.time.LocalDate;
+import java.time.Period;
+
 public class Main {
 
     public static void main(String[] args) {
+        if(majority(26, 5, 1997)){
+            System.out.println("Pełnoletni.");
+        } else {
+            System.out.println("Niepełnoletni.");
+        }
+    }
 
+    public static boolean majority(int day, int month, int year){
+        LocalDate birthDate = LocalDate.of(day, month, year);
+        LocalDate now = LocalDate.now();
+        Period period = Period.between(birthDate, now);
+        if (period.getYears() < 18) return false;
+        return true;
     }
 }
